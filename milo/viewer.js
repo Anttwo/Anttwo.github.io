f9ea2ff4dfc9b0a5c98cdf9d88bd8550944af003
Anttwo <antoine.guedon@hotmail.fr>
2025-12-10 04:21:17 +0100

    proxy change

--- milo/viewer.js
+++ milo/viewer.js
@@ -23,11 +23,11 @@ export class MiloViewer {
         this.splatLoaded = false;
         
         // URLs
-        this.corsProxy = "https://corsproxy.io/?";
+        // this.corsProxy = "https://corsproxy.io/?";
         // this.plyMeshUrl = this.corsProxy + `https://imagine.enpc.fr/~antoine.guedon/milo/${sceneConfig.name}/mesh.ply`;
         // this.splatUrl = this.corsProxy + `https://imagine.enpc.fr/~antoine.guedon/milo/${sceneConfig.name}/gs.ply`;
-        this.plyMeshUrl = this.corsProxy + `https://antoineguedon.fr/milo_gallery/${sceneConfig.name}/mesh.ply`;
-        this.splatUrl = this.corsProxy + `https://antoineguedon.fr/milo_gallery/${sceneConfig.name}/gs.ply`;
+        this.plyMeshUrl = `https://antoineguedon.fr/milo_gallery/${sceneConfig.name}/mesh.ply`;
+        this.splatUrl = `https://antoineguedon.fr/milo_gallery/${sceneConfig.name}/gs.ply`;
         
         // Transformation parameters
         this.transforms = {
